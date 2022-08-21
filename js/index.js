f4a866c2e93d4e6f5b7fd8c4be77d5f716c87cb5
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-22 03:30:21 +0600

    player list id issue fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
 const playerList = document.getElementById("player-list");
 
+const firstPlayer = document.getElementById("select-1");
+const secondPlayer = document.getElementById("select-2");
+const thirdPlayer = document.getElementById("select-3");
+const forthPlayer = document.getElementById("select-4");
+const fifthPlayer = document.getElementById("select-5");
+const sixthPlayer = document.getElementById("select-6");
+
+firstPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-1");
+  event.target.classList.add("disabled");
+});
+
+secondPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-2");
+  event.target.classList.add("disabled");
+});
+
+thirdPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-3");
+  event.target.classList.add("disabled");
+});
+
+forthPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-4");
+  event.target.classList.add("disabled");
+});
+
+fifthPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-5");
+  event.target.classList.add("disabled");
+});
+
+sixthPlayer.addEventListener("click", function (event) {
+  addPlayerToPlayerList("player-6");
+  event.target.classList.add("disabled");
+});
+
 function addPlayerToPlayerList(playerId) {
   if (playerList.childElementCount >= 5) {
     alert("You can not add more then 5 players in the list");
